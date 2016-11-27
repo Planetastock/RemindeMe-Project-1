c2e9ed46100d5cad363c85dde65c748b69e44932
Planetastock <planetastock@gmail.com>
2016-11-27 17:12:40 +0200

    resave project, lesson 9

--- app/src/main/java/com/planetastock/remindme/MainActivity.java
+++ app/src/main/java/com/planetastock/remindme/MainActivity.java
@@ -1,15 +1,14 @@
 package com.planetastock.remindme;
 
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TableLayout;
 
 import com.planetastock.remindme.adapter.TabsPagerFragmentAdapter;
 
